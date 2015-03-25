ba2424cbada8cb826cc0ac0ac737c223a816e2bb
Ng Wei Ern <ng.wei.ern@gmail.com>
2015-03-25 22:57:08 +0800

    created unit testing driver

---
+++ TMParserTest/TMParserTest.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TMParserTest{	
+
+	TEST_CLASS(UnitTest1){
+
+	public:
+
+		TEST_METHOD(timeTo24HFormatTest)
+		{
+            TMParser *parser = TMParser::getInstance(); 
+			Assert::AreEqual((std::string)"1200",parser->timeTo24HFormat("12pm"));
+		}
+
+	};
+}
