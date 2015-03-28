ac2f30a429300f40170ede41c389a8898a4237c9
Ng Xian Loon <sharonngxianloon@hotmail.com>
2015-03-28 11:19:47 +0800

    Designed interface

--- TMGUI/TMGUserInterface.h
+++ TMGUI/TMGUserInterface.h
@@ -49,6 +49,11 @@ namespace TMGUI {
 	private: System::Windows::Forms::TextBox^  userInput;
 	private: System::Windows::Forms::Label^  welcomeMessage;
 	private: System::Windows::Forms::RichTextBox^  richTextBox1;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::RichTextBox^  richTextBox2;
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::Label^  label4;
 	protected: 
 
 
@@ -69,14 +74,21 @@ namespace TMGUI {
 			this->userInput = (gcnew System::Windows::Forms::TextBox());
 			this->welcomeMessage = (gcnew System::Windows::Forms::Label());
 			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->richTextBox2 = (gcnew System::Windows::Forms::RichTextBox());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// userInput
 			// 
-			this->userInput->Location = System::Drawing::Point(99, 694);
-			this->userInput->Margin = System::Windows::Forms::Padding(4);
+			this->userInput->BackColor = System::Drawing::SystemColors::Window;
+			this->userInput->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->userInput->Location = System::Drawing::Point(173, 538);
+			this->userInput->Margin = System::Windows::Forms::Padding(2);
 			this->userInput->Name = L"userInput";
-			this->userInput->Size = System::Drawing::Size(932, 31);
+			this->userInput->Size = System::Drawing::Size(674, 20);
 			this->userInput->TabIndex = 1;
 			this->userInput->TextChanged += gcnew System::EventHandler(this, &TMGUserInterface::userInput_TextChanged);
 			this->userInput->KeyPress += gcnew System::Windows::Forms::KeyPressEventHandler(this, &TMGUserInterface::userInput_KeyPress);
@@ -84,30 +96,103 @@ namespace TMGUI {
 			// welcomeMessage
 			// 
 			this->welcomeMessage->AutoSize = true;
-			this->welcomeMessage->Location = System::Drawing::Point(94, 653);
-			this->welcomeMessage->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->welcomeMessage->Font = (gcnew System::Drawing::Font(L"Segoe UI", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(0)));
+			this->welcomeMessage->Location = System::Drawing::Point(30, 526);
+			this->welcomeMessage->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->welcomeMessage->Name = L"welcomeMessage";
-			this->welcomeMessage->Size = System::Drawing::Size(530, 25);
+			this->welcomeMessage->Size = System::Drawing::Size(131, 32);
 			this->welcomeMessage->TabIndex = 2;
-			this->welcomeMessage->Text = L"Welcome to TimeMaster. Please enter your command:";
+			this->welcomeMessage->Text = L"Command:";
+			this->welcomeMessage->Click += gcnew System::EventHandler(this, &TMGUserInterface::welcomeMessage_Click);
 			// 
 			// richTextBox1
 			// 
-			this->richTextBox1->Location = System::Drawing::Point(99, 54);
+			this->richTextBox1->BackColor = System::Drawing::Color::LightCyan;
+			this->richTextBox1->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->richTextBox1->Location = System::Drawing::Point(35, 47);
+			this->richTextBox1->Margin = System::Windows::Forms::Padding(2);
 			this->richTextBox1->Name = L"richTextBox1";
-			this->richTextBox1->Size = System::Drawing::Size(1138, 578);
+			this->richTextBox1->Size = System::Drawing::Size(812, 344);
 			this->richTextBox1->TabIndex = 3;
 			this->richTextBox1->Text = L"";
 			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Segoe UI Symbol", 18, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(194, 14);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(141, 32);
+			this->label1->TabIndex = 4;
+			this->label1->Text = L"TimeMaster";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Segoe UI Symbol", 11.25F, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(0)));
+			this->label2->Location = System::Drawing::Point(46, 25);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(153, 20);
+			this->label2->TabIndex = 5;
+			this->label2->Text = L"Master your time with";
+			// 
+			// richTextBox2
+			// 
+			this->richTextBox2->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->richTextBox2->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->richTextBox2->Font = (gcnew System::Drawing::Font(L"Segoe UI", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(0)));
+			this->richTextBox2->ForeColor = System::Drawing::SystemColors::Window;
+			this->richTextBox2->Location = System::Drawing::Point(173, 409);
+			this->richTextBox2->Name = L"richTextBox2";
+			this->richTextBox2->ReadOnly = true;
+			this->richTextBox2->Size = System::Drawing::Size(674, 107);
+			this->richTextBox2->TabIndex = 6;
+			this->richTextBox2->Text = L"";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Segoe UI", 21.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(0)));
+			this->label3->Location = System::Drawing::Point(61, 444);
+			this->label3->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(101, 40);
+			this->label3->TabIndex = 7;
+			this->label3->Text = L"Status:";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->BackColor = System::Drawing::Color::Transparent;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Segoe UI", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(0)));
+			this->label4->ForeColor = System::Drawing::Color::Black;
+			this->label4->Location = System::Drawing::Point(767, 25);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(80, 20);
+			this->label4->TabIndex = 8;
+			this->label4->Text = L"Overdue: 6";
+			// 
 			// TMGUserInterface
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(12, 25);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1337, 881);
+			this->BackColor = System::Drawing::Color::Teal;
+			this->ClientSize = System::Drawing::Size(879, 592);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->richTextBox2);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
 			this->Controls->Add(this->richTextBox1);
 			this->Controls->Add(this->welcomeMessage);
 			this->Controls->Add(this->userInput);
-			this->Margin = System::Windows::Forms::Padding(4);
+			this->Margin = System::Windows::Forms::Padding(2);
 			this->MaximizeBox = false;
 			this->Name = L"TMGUserInterface";
 			this->Text = L"TMGUserInterface";
@@ -147,7 +232,18 @@ namespace TMGUI {
 			 }
 	private: System::Void displayScreen_TextChanged(System::Object^  sender, System::EventArgs^  e) {
 			 }
-	private: System::Void userInput_TextChanged(System::Object^  sender, System::EventArgs^  e) {
+
+
+	private: System::Void welcomeMessage_Click(System::Object^  sender, System::EventArgs^  e) {
 		 }
+
+	private: System::Void userInput_TextChanged(System::Object^  sender, System::EventArgs^  e) {
+				 if (userInput->Text == "add") {
+					richTextBox2->Text = "add <TIMEMARKER> <BLA>";
+				 } else {
+					 richTextBox2->Text = "";
+				 }
+			 }
+
 };
 }
